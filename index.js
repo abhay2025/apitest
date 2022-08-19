a5ad1751033f380dcf802f1275e74c758035e387
abhay2025 <69841673+abhay2025@users.noreply.github.com>
2022-08-19 13:53:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -25,3 +25,31 @@ const apiusersschema = new mongoose.Schema({
 const apiusers = mongoose.model("apiusers", apiusersschema);
 
 app.get('/', (req, res) => { res.send('hello') });
+app.get('/user/login', async (req, res) => {
+    try {
+        const { username, password } = req.query;
+        if (username == undefined || password == undefined)
+            res.send('Please fill the fields')
+        else if (username == "" || password == "")
+            res.send('Please fill the fields')
+        else {
+
+            const user2 = await apiusers.findOne({
+                username,
+                password
+            })
+            if (user2 == null)
+                res.status(401).send("Invalid Credentials");
+            else {
+                res.send(user2);
+
+            }
+        }
+    }
+
+    catch (err) {
+        console.log(err);
+        res.status(500).send("Server Error");
+    }
+
+})
